ef82772ba4dda9a7b1ef060ae7543d9951bb145f
mohitcse88 <mohitkumardoctor@gmail.com>
2024-12-25 20:56:29 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "Mohit"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => Nan
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = NaN
+// let isLoggedIn = null
+// let isLoggedIn = undefined
+// let isLoggedIn = ""
+let isLoggedIn = "Mohit"
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false 
+// "", NaN, null, undefined => false
+
+let someNumber = 33
+let someNumberInString = String(someNumber)
+
+console.log(someNumberInString);
+console.log(typeof someNumberInString);
