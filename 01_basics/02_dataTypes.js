b6a7fb2f39a7857bd2761872211c453a2875a62b
mohitcse88 <mohitkumardoctor@gmail.com>
2024-12-25 15:14:39 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log( 3 
+    +
+    3 ); // Code readablity should be high
+
+console.log("Mohit");
+
+
+let name = "Mohit"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// Number => 2 to power 53 
+// BigInt => 
+// String => "", ''
+// Boolean => true / false
+// Null => standalone value / It is representation of empty value
+// Undefined => Variable is declared but value is not assigned 
+// symbol => Unique
+
+
+// Object
+
+console.log(typeof "Mohit");
+console.log(typeof age);
+console.log(typeof null); // Object
+console.log(typeof undefined); // undefined
+
+// https://tc39.es/ecma262/#sec-ecmascript-language-types-undefined-type
+
+
