ec2b9403d69b0581682fd838e68c2f56388761a7
mohitcse88 <mohitkumardoctor@gmail.com>
2024-12-26 06:30:14 +0530

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -35,5 +35,61 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 let someNumberInString = String(someNumber)
 
-console.log(someNumberInString);
-console.log(typeof someNumberInString);
+// console.log(someNumberInString);
+// console.log(typeof someNumberInString);
+
+
+// ************************ Operations ****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Mohit"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+// link to study 
+// https://tc39.es/ecma262/multipage/abstract-operations.html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
